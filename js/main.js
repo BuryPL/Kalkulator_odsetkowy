6560247db27b015916a0c18fc81e85d1f1f17180
Krzysztof Burkot <buryPL@gmail.com>
2017-04-08 12:20:14 +0200

    Kalkulator_odsetek_done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function obliczZysk(wplata,iloscLat,okresKapitalizacji, oprocentowanie, podatek) {
+    if (podatek) {
+        var oprocentowanieOpodatkowane = oprocentowanie * 0.89;
+    }else{
+        var oprocentowanieOpodatkowane = oprocentowanie;
+    }
+    var kapital = wplata*Math.pow(1+(oprocentowanieOpodatkowane/okresKapitalizacji),(iloscLat*okresKapitalizacji));
+//    return (parseFloat("kapital").toFixed(2));
+    return kapital;
+}
+
+function oblicz() {
+    var wplata = parseInt(document.getElementById("wplata").value);
+    var iloscLat = parseInt(document.getElementById("ilosc_lat").value);
+    var okresKapitalizacji = parseInt(document.getElementById("okres_kapitalizacji").value);
+    var oprocentowanie = parseInt(document.getElementById("oprocentowanie").value)/100;
+    var podatek = document.getElementById("podatek").checked;
+    var wynik = obliczZysk(wplata,iloscLat,okresKapitalizacji,oprocentowanie,podatek);
+    console.log(wynik);
+    document.getElementById("wynik").innerHTML = wynik;
+}
