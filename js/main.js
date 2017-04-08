aa6fd92866067450fda429c74b745f82042e32a5
Krzysztof Burkot <buryPL@gmail.com>
2017-04-08 10:31:52 +0200

    Init

---
+++ js/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function kalkulator() {
+    var wsad = document.getElementById('input_math').value;   
+    var tablicaWsadowa = Array.from(wsad);
+    console.log(tablicaWsadowa);
+}
